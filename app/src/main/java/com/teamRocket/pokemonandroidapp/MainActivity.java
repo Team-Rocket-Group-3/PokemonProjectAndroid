9850be276a9d2c272dc2d1438f39ac031b3ab813
SquidoCode <squidocode@hotmail.com>
2021-04-09 14:29:10 +0200

    Teams without format

--- app/src/main/java/com/teamRocket/pokemonandroidapp/MainActivity.java
+++ app/src/main/java/com/teamRocket/pokemonandroidapp/MainActivity.java
@@ -14,8 +14,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        Intent navegar = new Intent(getBaseContext(), LstPokemonView.class);
-        Intent navegar = new Intent(getBaseContext(), LstTeamsActivity.class);
+        Intent navegar = new Intent(getBaseContext(), LstPokemonView.class);
+//        Intent navegar = new Intent(getBaseContext(), LstTeamsActivity.class);
         startActivity(navegar);
     }
 }
